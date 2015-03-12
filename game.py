3e3a07c034f0a6389a9eef4602c786fd6f42531f
Warren <warren@701search.com>
2015-03-12 09:46:09 +0800

    Fix bugs

--- game.py
+++ game.py
@@ -28,30 +28,35 @@ class Game(object):
             guess = raw_input("Player '%s' to answer:" % players_name)
 
             # check for a correct answer
-            points = self.check_answer(guess)
+            points = self.get_points(guess)
 
-            if points > 0:  # if answer matched something, then assign the points to current player
-                if players_name in self.points:
-                    self.points[players_name] += points
-                else:
-                    self.points[players_name] = points
-            else:  # add +1 to players mistake
-                if players_name in self.mistakes:
-                    self.mistakes[players_name] += 1
-                else:
-                    self.mistakes[players_name] = 1
-
-                # remove player if mistake has reached the limit
-                if self.mistakes[players_name] >= 3:
-                    self.remove_player()
-
-                # get the next player
-                self.get_next_player()
+            self.set_scores(points)
 
             is_winner = self.check_winner()
 
             if is_winner:
                 self.end(self.current_player)
+
+    def set_scores(self, points):
+        players_name = self.players[self.current_player]
+
+        if points > 0:  # if answer matched something, then assign the points to current player
+            if players_name in self.points:
+                self.points[players_name] += points
+            else:
+                self.points[players_name] = points
+        else:  # add +1 to players mistake
+            if players_name in self.mistakes:
+                self.mistakes[players_name] += 1
+            else:
+                self.mistakes[players_name] = 1
+
+            # remove player if mistake has reached the limit
+            if self.mistakes[players_name] >= 3:
+                self.remove_player()
+            else:
+                # get the next player
+                self.get_next_player()
 
     def get_next_player(self):
         self.current_player += 1
@@ -60,18 +65,21 @@ class Game(object):
             self.current_player = 0
 
     def remove_player(self):
+        print "Player '%s' is eliminated." % self.players[self.current_player]
         self.players.remove(self.players[self.current_player])
 
-    def check_answer(self, guess):
-        i = 0
+    def get_points(self, guess):
         positions = []
 
         # Check for multiple character guesses
-        if guess.lower() in self.guess_list:
-            print "The character '%s' has already been entered." % guess.lower()
-            return 0
+        if (self.check_duplicate_guess(guess.lower())!=True):
+            # loop through the characters in self.word
+            positions = self.check_for_matches(guess, positions)
 
-        # loop through the characters in self.word
+        return len(positions)
+
+    def check_for_matches(self, guess, positions):
+        i = 0
         for c in self.word:
             # check each character if equal to the guessed character and not in self.guess_list
             if c.lower() == guess.lower():
@@ -82,10 +90,10 @@ class Game(object):
 
         # if the guessed character matched at least one in the self.word
         if len(positions) > 0:
-            i = 0
             redacted_word_array = list(self.redacted_word)
 
             # replace the self.redacted_word with the correct guess
+            i = 0
             for c in self.redacted_word:
                 if i in positions:
                     redacted_word_array[i] = guess.upper()
@@ -93,7 +101,12 @@ class Game(object):
 
             self.redacted_word = "".join(redacted_word_array)
 
-        return len(positions)
+        return positions
+
+    def check_duplicate_guess(self, guess):
+        if guess in self.guess_list:
+            print "The character '%s' has already been entered." % guess.lower()
+            return True
 
     def check_winner(self):
         if '*' not in list(self.redacted_word):
@@ -117,6 +130,6 @@ class Game(object):
             print "%s | %s | %s |" % (name.ljust(7), str(points).ljust(6), str(mistakes).ljust(8))
 
     def end(self, current_player):
-        print "The word is %s" % self.word.upper()
+        print "The word is -==%s==-" % self.word.upper()
         print "The winner is \"%s\"!" % self.players[current_player]
         exit()
